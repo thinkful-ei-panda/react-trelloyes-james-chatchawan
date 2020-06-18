033d29aa4f8ee85ac501c19fbc4715606e2335f4
jenthura <joshuasth@gmail.com>
2020-06-18 11:40:48 -0500

    finished state-shopping

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import STORE from './store'
+// import STORE from './store'
 
-console.log(STORE);
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/state-shopping/AddItemForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default class AddItemForm extends React.Component {
+  
+  onSubmitForm = (e) => {
+    e.preventDefault()
+    // e.target['inputName'] references `<input name='inputName' />`
+    this.props.onAddItem(e.target.itemToAdd.value)
+  }
+  
+  render() {
+    /* TODO: need to wire up the form to add items in App state... */
+    return (
+      <form  onSubmit={this.onSubmitForm}>
+        <input
+          name="itemToAdd"
+          type="text"
+          placeholder="carrots"
+          aria-label="Shopping list item"
+        />
+        <button type="submit">Add Item</button>
+      </form>
+    );
+  }
+}
